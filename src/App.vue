a8f76694304a76d50a4b20bd20e94eafeefb37c5
Ron Walker <ron22@hey.com>
2022-02-24 16:39:41 -0800

    remove hardcoded user

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <my-portfolio msg="My Crypto Portfolio" email="rondwalker22@yahoo.com" 
-    auth="{{location.href.split('#')[1]).split('&')[1].split('=')[1]}}}"
-    jwt="love"
+    <my-portfolio msg="My Crypto Portfolio"
+    :auth="window"
+    :email="getEmail()"
     />
   </div>
 </template>
@@ -14,6 +14,31 @@ export default {
   name: 'App',
   components: {
     MyPortfolio
+  },
+  data() {
+      return {
+          window: (window.location.href.split('#')[1]).split('&')[1].split('=')[1]
+      }
+  },
+    methods: {
+    getEmail() {
+        var idTokenWithEquals = (location.href.split('#')[1]).split('&')[0];
+        var COGNITO_ID_TOKEN = idTokenWithEquals.split('=')[1];
+        COGNITO_ID_TOKEN = this.parseJwt(COGNITO_ID_TOKEN);
+
+        return COGNITO_ID_TOKEN.email;
+
+    },
+    parseJwt(token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+    },
+
   }
 }
 </script>
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>{{ msg }}</h1>
     <body>
+        <a href="https://my-coins.auth.us-east-2.amazoncognito.com/login?client_id=n2l1crskuigpqp3lrbsj8r7nj&response_type=token&scope=email+openid&redirect_uri=http://localhost:8080">Login</a>
         <div>
             <div></div>
             <input v-model="newCoin.name" placeholder="coin name">
@@ -37,7 +38,7 @@
 </template>
 
 <script>
-
+// https://my-coins.auth.us-east-2.amazoncognito.com/login?client_id=n2l1crskuigpqp3lrbsj8r7nj&response_type=token&scope=email+openid&redirect_uri=http://localhost:8080/
 import axios from 'axios';
 
 export default {
@@ -45,7 +46,7 @@ export default {
   props: {
     msg: String,
     auth: String,
-    jwt: String,
+    email: String
   },
 
   data: function() {
@@ -57,13 +58,13 @@ export default {
               amount: null,
           },
           userEmail: "",
-          url: `https://t3d210uhn7.execute-api.us-east-2.amazonaws.com/test/portfolio?emailId=${this.userEmail}`
+          url: `https://t3d210uhn7.execute-api.us-east-2.amazonaws.com/test/portfolio?emailId=${this.email}`
       }
   },
   mounted () {
       this.state = "loading"
+      this.getEmail();
       this.getCoin();
-      this.parseJwt();
   },
   methods: {
     getCoin() {
@@ -79,15 +80,24 @@ export default {
         }
         )
     },
-    parseJwt() {
-        var base64Url = this.auth.split('.')[1];
+    getEmail() {
+        var idTokenWithEquals = (location.href.split('#')[1]).split('&')[0];
+        var COGNITO_ID_TOKEN = idTokenWithEquals.split('=')[1];
+        COGNITO_ID_TOKEN = this.parseJwt(COGNITO_ID_TOKEN);
+
+        this.userEmail = COGNITO_ID_TOKEN.email 
+
+    },
+    parseJwt(token) {
+        var base64Url = token.split('.')[1];
         var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
         var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
 
-        this.userEmail = JSON.parse(jsonPayload);
+        return JSON.parse(jsonPayload);
     },
+
     updateCoin() {
         this.state = "loading";
         if (this.newCoin.name === null || this.newCoin.amount === null) {
